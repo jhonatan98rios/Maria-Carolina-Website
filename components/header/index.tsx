ad2443c8d636318c53f4d65e936d0676f34b91e4
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-06-10 00:47:50 -0300

    header mobile

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,18 +5,21 @@ export default function Header() {
     <header className={styles.header}>
       <span className={styles.goldbg} />
 
-      <div className={'container flex'}>
-        <div className="flex w-40">
-          <h2 className="font-primary fs-48 lh-56">
-            Precisa de um <br/> <strong> Advogado? </strong>  <br/>
+      <div className="container flex flex-column-tablet relative w-100 p-r-32 p-l-32">
+        <div className="flex w-40 w-100-tablet">
+          <h2 className="font-primary fs-48 lh-56 flex flex-column jc-end">
+            Precisa de um <br/> <strong> Advogado? </strong>
             <span className="color-light m-t-32 flex"> Clique aqui <br/> agora mesmo </span>
           </h2>
         </div>
 
-        <div className="w-60">
+        <div className="w-60 w-100-tablet">
           <img className={styles.image} src="./header/athemis.png" alt=""/>
         </div>
       </div>
+      <a href="./" target="_blank">
+        <img className={styles.whatsapp} src="./header/whatsapp.png" alt="icone do whatsapp"/>
+      </a>
     </header>
   )
 }
