8c57ad6ec52db7bc026f3fe8119a7ae85b9d53d0
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-06-09 23:55:17 -0300

    desktop header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,24 +3,20 @@ import styles from './style.module.scss'
 export default function Header() {
   return (
     <header className={styles.header}>
-
       <span className={styles.goldbg} />
 
       <div className={'container flex'}>
-
-        <div>
-          <h2>
-            Precisa de um Advogado? 
-            <span className="color-light"> Clique aqui agora mesmo </span>
+        <div className="flex w-40">
+          <h2 className="font-primary fs-48 lh-56">
+            Precisa de um <br/> <strong> Advogado? </strong>  <br/>
+            <span className="color-light m-t-32 flex"> Clique aqui <br/> agora mesmo </span>
           </h2>
         </div>
 
-        <div>
+        <div className="w-60">
           <img className={styles.image} src="./header/athemis.png" alt=""/>
         </div>
-
       </div>
-
     </header>
   )
 }
